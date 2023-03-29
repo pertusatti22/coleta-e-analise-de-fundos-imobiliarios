abdccb43d78c2618bf7bbd53af310ea1caad7aff
pertusatti22 <pertusatti22@gmail.com>
2023-03-29 15:38:39 -0300

    Add CSV save

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
-
 # Pacotes
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
@@ -22,24 +22,36 @@ allTicker = []
 getTicker = driver.find_elements(By.CLASS_NAME, "tickerBox__title")
 for ticker in getTicker:
     allTicker.append(ticker.text)
+print(allTicker)
+print(len(allTicker))
 print('Lista de FIIs OK')
 
 # Filtro
 funds = []
-maximo = len(allTicker)
+maximo = 50  # len(allTicker)
 for i in range(0, maximo):
     url = "https://fiis.com.br/" + allTicker[i] + "/"
     driver.get(url)
     statement = [allTicker[i]]
-    valor_patrimonial = driver.find_element(by=By.XPATH, value="//*[@id=\"carbon_fields_fiis_quotations_chart-2\"]/div[2]/div[3]/p[1]/b").get_attribute('innerText').replace('.', '').replace(',', '.')
+    valor_patrimonial = driver.find_element(by=By.XPATH,
+                                            value="//*[@id=\"carbon_fields_fiis_quotations_chart-2\"]/div[2]/div[3]/p[1]/b").get_attribute(
+        'innerText').replace('.', '').replace(',', '.')
     statement.append(float(valor_patrimonial))
-    cotacao = driver.find_element(by=By.XPATH, value="//*[@id=\"carbon_fields_fiis_quotations_chart-2\"]/div[1]/div[2]/div[1]/div[1]/span[2]").get_attribute("textContent").replace('.', '').replace(',', '.')
+    cotacao = driver.find_element(by=By.XPATH,
+                                  value="//*[@id=\"carbon_fields_fiis_quotations_chart-2\"]/div[1]/div[2]/div[1]/div[1]/span[2]").get_attribute(
+        "innerText").replace('.', '').replace(',', '.')
     statement.append(float(cotacao))
-    patrimonio_liquido = driver.find_element(by=By.XPATH, value="//*[@id=\"carbon_fields_fiis_header-2\"]/div/div/div[1]/div[2]/div/div[3]/p[1]/b").get_attribute("textContent").split(' ')
-    statement.append(patrimonio_liquido[0])
-    rendimento = driver.find_element(by=By.XPATH, value="//*[@id=\"carbon_fields_fiis_header-2\"]/div/div/div[1]/div[2]/div/div[2]/p[1]/b").get_attribute("textContent").replace('.', '').replace(',', '.')
+    patrimonio_liquido = driver.find_element(by=By.XPATH,
+                                             value="//*[@id=\"carbon_fields_fiis_header-2\"]/div/div/div[1]/div[2]/div/div[3]/p[1]/b").get_attribute(
+        "innerText")
+    statement.append(patrimonio_liquido)
+    rendimento = driver.find_element(by=By.XPATH,
+                                     value="//*[@id=\"carbon_fields_fiis_header-2\"]/div/div/div[1]/div[2]/div/div[2]/p[1]/b").get_attribute(
+        "innerText").replace('.', '').replace(',', '.')
     statement.append(float(rendimento))
-    dividend_yield = driver.find_element(By.XPATH, "//*[@id=\"carbon_fields_fiis_header-2\"]/div/div/div[1]/div[2]/div/div[1]/p[1]/b").get_attribute("textContent").replace('.', '').replace(',', '.')
+    dividend_yield = driver.find_element(By.XPATH,
+                                         "//*[@id=\"carbon_fields_fiis_header-2\"]/div/div/div[1]/div[2]/div/div[1]/p[1]/b").get_attribute(
+        "innerText").replace('.', '').replace(',', '.')
     statement.append(float(dividend_yield))
     print('Dados Capturados - Valor Patrimonial, Cotação, Patrimônio Líquido, Dividend Yield')
 
@@ -50,9 +62,9 @@ for i in range(0, maximo):
     # PL > 1 Bi
     condicao2 = 'B' in statement[3]
     # Rend > 0,70 centavos
-    condicao3 = statement[4] > 0.7
+    condicao3 = statement[4] > 0.5
     # DY >= 0.6%
-    condicao4 = statement[5] > 0.6
+    condicao4 = statement[5] / 12 > 0.6
     filtro = condicao1 and condicao2 and condicao3 and condicao4
     print('Teste condicional aplicado')
     if filtro:
@@ -64,4 +76,9 @@ for i in range(0, maximo):
 # Apresentação
 print(len(funds))
 print(funds)
-print('Tarefa executada com sucesso!')
+
+dataframe = pd.DataFrame(funds, columns=['Ticker', 'VP', 'COT', 'PL', 'REND', 'DY'])
+print(dataframe)
+dataframe.to_csv('FII.csv')
+
+print('Análise concluída com sucesso!')
