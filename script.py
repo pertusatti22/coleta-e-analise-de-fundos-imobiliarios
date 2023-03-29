f8845fcb700912d42abed1e0d65eec708a8075e8
pertusatti22 <pertusatti22@gmail.com>
2023-03-29 14:23:44 -0300

    Update nos Xpaths e no driver

--- script.py
+++ script.py
@@ -1,55 +1,49 @@
-# Etapa 1
-!apt update
-!apt install chromium - chromedriver
-!pip install selenium
-print('Dependências Instaladas')
 
-# Etapa 2
+# Pacotes
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 
-print('Bibliotecas carregadas')
+print('Pacotes OK')
 
-# Etapa 3
-# Configurando web scraping
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')  # Run in headless mode, i.e., without a UI or display server dependencies.
-chrome_options.add_argument('--no-sandbox')  # Disables the sandbox for all process types that are normally sandboxed. Meant to be used as a browser-level switch for testing purposes only. https://www.google.com/googlebooks/chrome/med_26.html
-chrome_options.add_argument('--disable-dev-shm-usage')  # The /dev/shm partition is too small in certain VM environments, causing Chrome to fail or crash (see http://crbug.com/715363). Use this flag to work-around this issue (a temporary directory will always be used to create anonymous shared memory files).
-wd_chrome = webdriver.Chrome(chrome_options=chrome_options)
-print('Driver configurado')
+# Configurando o Driver
+service = Service(r'C:\Program Files\Mozilla Firefox\geckodriver.exe')
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Firefox(service=service, options=options)
+print('Driver OK')
 
-# Etapa 4
+# Listando FIIs
 url = "https://fiis.com.br/lista-de-fundos-imobiliarios/"
-wd_chrome.get(url)
+driver.get(url)
 print('URL configurada')
 allTicker = []
-getTicker = wd_chrome.find_elements(By.CLASS_NAME, "ticker")
+getTicker = driver.find_elements(By.CLASS_NAME, "tickerBox__title")
 for ticker in getTicker:
     allTicker.append(ticker.text)
-print('Lista de FIIs carregada')
+print('Lista de FIIs OK')
 
-# Etapa 5
+# Filtro
 funds = []
-max = len(allTicker)  # Esse parâmetro determina pro python quantos fiis ele vai analisar da lista
-for i in range(0, max):
+maximo = len(allTicker)
+for i in range(0, maximo):
     url = "https://fiis.com.br/" + allTicker[i] + "/"
-    wd_chrome.get(url)
-    statement = []
-    statement.append(allTicker[i])
-    vp = wd_chrome.find_element(By.XPATH, "/html/body/div[4]/section[2]/div/table/tbody/tr/td[4]/h3[1]").get_attribute("textContent").replace('.', '').replace(',', '.').split('R$')
-    statement.append(float(vp[1]))
-    cot = wd_chrome.find_element(By.XPATH, "/html/body/div[4]/section[3]/div/div/div[3]/div/div[1]/span[2]").get_attribute("textContent").replace('.', '').replace(',', '.')
-    statement.append(float(cot))
-    pl = wd_chrome.find_element(By.XPATH, "/html/body/div[4]/section[2]/div[1]/table/tbody/tr/td[3]/h3[1]").get_attribute("textContent").split(' ')
-    statement.append(pl[1])
-    rend = wd_chrome.find_element(By.XPATH, "/html/body/div[4]/section[2]/div[1]/table/tbody/tr/td[2]/h3[1]").get_attribute("textContent").replace('.', '').replace(',', '.').split('R$')
-    statement.append(float(rend[1]))
-    dy = wd_chrome.find_element(By.XPATH, "/html/body/div[4]/section[2]/div[1]/table/tbody/tr/td[1]/h3[1]").get_attribute("textContent").split('%')
-    statement.append(float(dy[0].replace(',', '.')))
-    print('Dados Capturados - VP, Cot, PL, Dy')
+    driver.get(url)
+    statement = [allTicker[i]]
+    valor_patrimonial = driver.find_element(by=By.XPATH, value="//*[@id=\"carbon_fields_fiis_quotations_chart-2\"]/div[2]/div[3]/p[1]/b").get_attribute('innerText').replace('.', '').replace(',', '.')
+    statement.append(float(valor_patrimonial))
+    cotacao = driver.find_element(by=By.XPATH, value="//*[@id=\"carbon_fields_fiis_quotations_chart-2\"]/div[1]/div[2]/div[1]/div[1]/span[2]").get_attribute("textContent").replace('.', '').replace(',', '.')
+    statement.append(float(cotacao))
+    patrimonio_liquido = driver.find_element(by=By.XPATH, value="//*[@id=\"carbon_fields_fiis_header-2\"]/div/div/div[1]/div[2]/div/div[3]/p[1]/b").get_attribute("textContent").split(' ')
+    statement.append(patrimonio_liquido[0])
+    rendimento = driver.find_element(by=By.XPATH, value="//*[@id=\"carbon_fields_fiis_header-2\"]/div/div/div[1]/div[2]/div/div[2]/p[1]/b").get_attribute("textContent").replace('.', '').replace(',', '.')
+    statement.append(float(rendimento))
+    dividend_yield = driver.find_element(By.XPATH, "//*[@id=\"carbon_fields_fiis_header-2\"]/div/div/div[1]/div[2]/div/div[1]/p[1]/b").get_attribute("textContent").replace('.', '').replace(',', '.')
+    statement.append(float(dividend_yield))
+    print('Dados Capturados - Valor Patrimonial, Cotação, Patrimônio Líquido, Dividend Yield')
 
-    # Etapa 6
+    # Seleção de FIIs
     print('Aplicando filtro em {}'.format(allTicker[i]))
     # VP > Cot
     condicao1 = statement[1] > statement[2]
@@ -67,7 +61,7 @@ for i in range(0, max):
     else:
         print('FII não atende requisitos!')
 
-# Etapa 7
+# Apresentação
 print(len(funds))
 print(funds)
 print('Tarefa executada com sucesso!')
